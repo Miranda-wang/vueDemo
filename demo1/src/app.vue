a49e00b966e34de649ad3888359f1ebccd4c4419
wanghuili <huili876@163.com>
2018-05-18 06:11:30 +0800

    click method two

--- demo1/src/app.vue
+++ demo1/src/app.vue
@@ -2,7 +2,7 @@
     <div>
         {{msg}}
           <ul>
-            <li v-for="(item,index) in todos" v-on:click="yang(index,item.id,item.clickNo)" :class="[item.isSelected?'clicked unclicked':'unclicked']"><span :id="item.id">{{item.text}}</span></li>
+            <li v-for="(item,index) in todos" v-on:click="click(index,item.id)" :class="[item.isSelected?'clicked unclicked':'unclicked']"><span :id="item.id">{{item.text}}</span></li>
         </ul>
         
     </div>
@@ -15,25 +15,32 @@
     export default {
         data(){
             return {
-                msg:"老不死的地球,你好!",
-                // clickNo:0,
                 clickResult:[],
-               todos:[
+               todos:
+                    // [
                     // id:记录当前点击的对象,clickNo记录当前对象点击的次数
-                     {id:1,text:'nihao ',isSelected:false,clickNo:0},
-                    {id:2,text:'woaini ',isSelected:false,clickNo:0},
-                    {id:3,text:'qinaide ',isSelected:false,clickNo:0},
-                    {id:4,text:'guniang ',isSelected:false,clickNo:0},
-                    {id:5,text:'qinaide ',isSelected:false,clickNo:0},
-                    {id:6,text:'guniang ',isSelected:false,clickNo:0},
-                    ],
+                    //  {id:1,text:'nihao ',isSelected:false,clickNo:0},
+                    // {id:2,text:'woaini ',isSelected:false,clickNo:0},
+                    // {id:3,text:'qinaide ',isSelected:false,clickNo:0},
+                    // {id:4,text:'guniang ',isSelected:false,clickNo:0},
+                    // {id:5,text:'qinaide ',isSelected:false,clickNo:0},
+                    // {id:6,text:'guniang ',isSelected:false,clickNo:0},
+                    // ],
+                    [
+                        {id:1,text:'nihao ',isSelected:false},
+                        {id:2,text:'woaini ',isSelected:false},
+                        {id:3,text:'qinaide ',isSelected:false},
+                        {id:4,text:'guniang ',isSelected:false},
+                        {id:5,text:'qinaide ',isSelected:false},
+                        {id:6,text:'guniang ',isSelected:false},
+                    ]
             }
         },
         methods:{
-            yang:function (index,itemId,itemClickNo) {
-                itemClickNo++;
+            // yang:function (index,itemId,itemClickNo) {
+                // itemClickNo++;
                 // 每次点击后存储点击的次数
-                this.todos[index].clickNo = itemClickNo;
+                // this.todos[index].clickNo = itemClickNo;
                 // 如果存储结果<3,
                 //     如果是奇数点击:
                 //          奇数点击添加背景效果;
@@ -47,32 +54,65 @@
                 //         偶数点击删除该点击对象;
                 //     谈对话框,
 
-                if(this.clickResult.length  < 3){
-                    if(this.todos[index].clickNo % 2 ==1 ){
-                        // 奇数点击添加背景效果
-                        this.todos[index].isSelected = true;
-                        // 存储奇数点击
-                         this.clickResult.push(itemId);
-                        console.log(this.clickResult);
-                    }else if (this.todos[index].clickNo % 2 ==0 ) {
-                        // 偶数点击移除背景效果
-                        this.todos[index].isSelected = false;
-                        // 删除偶数点击,并且更新数组
-                       this.clickResult = this.remove(this.clickResult,itemId); 
+                // if(this.clickResult.length  < 3){
+                //     if(this.todos[index].clickNo % 2 ==1 ){
+                //         // 奇数点击添加背景效果
+                //         this.todos[index].isSelected = true;
+                //         // 存储奇数点击
+                //          this.clickResult.push(itemId);
+                //         console.log(this.clickResult);
+                //     }else if (this.todos[index].clickNo % 2 ==0 ) {
+                //         // 偶数点击移除背景效果
+                //         this.todos[index].isSelected = false;
+                //         // 删除偶数点击,并且更新数组
+                //        this.clickResult = this.remove(this.clickResult,itemId); 
+                //     }
+                // }else if (this.clickResult.length ==3 ) {
+                //     if (this.todos[index].clickNo % 2 ==0 ) {
+                //         // 偶数点击移除背景效果
+                //         this.todos[index].isSelected = false;
+                //         // 删除偶数点击,并且更新存储数组,一定得存储,更新数组
+                //        this.clickResult = this.remove(this.clickResult,itemId); 
+                //        console.log(this.clickResult);
+                //     }else{
+                //          toastr.success('我是阿奇')
+                //     }   
+                // }  
+                // console.log(this.clickResult);
+            // }
+            arrRemove: function (arr, item){
+                    var result=[];  
+                    for(var i=0; i<arr.length; i++){  
+                    if(arr[i]!=item){  
+                        result.push(arr[i]);  
+                        // console.log(result);
+                        // console.log("itemid"+item);
+                        }  
                     }
-                }else if (this.clickResult.length ==3 ) {
-                    if (this.todos[index].clickNo % 2 ==0 ) {
-                        // 偶数点击移除背景效果
+                    return result;   
+            },
+            click:function (index,itemId) {
+                // 选中状态,长度<3
+                //     添加点击背景效果,
+                //     存储点击数据
+                    if( this.todos[index].isSelected == false  &&  this.clickResult.length <3 ) {
+                        this.todos[index].isSelected = true;
+                        this.clickResult.push(itemId);
+                        console.log(this.clickResult);
+                    }else if ( this.todos[index].isSelected == true) {
                         this.todos[index].isSelected = false;
-                        // 删除偶数点击,并且更新存储数组,一定得存储,更新数组
-                       this.clickResult = this.remove(this.clickResult,itemId); 
-                       console.log(this.clickResult);
-                    }else{
-                         toastr.success('我是阿奇')
-                    }   
-                }  
-                console.log(this.clickResult);
+                        this.clickResult = this.arrRemove( this.clickResult,itemId);
+                        console.log(this.clickResult);
+                    }else {
+                        toastr.success('我是阿奇');
+                    }
+                // 非选中状态
+                //     如果是之前选中的,需要取消背景效果,
+                //     删除存储的数据
+                //     如果不是之前选中的,也就是>=3,弹出提示
+
             }
+
         }
     }
 </script>
